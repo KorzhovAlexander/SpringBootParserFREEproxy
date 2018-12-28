1196dfd68386a2ef51c973608766f71580235c94
honey <honeyiamgood@mail.ru>
2018-12-28 23:56:08 +0300

    deploy prj

--- src/main/java/WebApp/Controller/RegistrationController.java
+++ src/main/java/WebApp/Controller/RegistrationController.java
@@ -25,7 +25,6 @@ public class RegistrationController {
 
     @PostMapping("/Register")
     public String AddUser(user user) {
-
         if (userService.createUser(user)) {
 //            userService.sendAndCreateActivationCode(user);
             return "redirect:/login";
